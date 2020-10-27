8b3b019c50eabbce6ac03d61069893021733a963
Amarjeet Singh <banwait.amarjeet@gmail.com>
2020-10-27 22:00:54 +1000

    Add project files.

---
+++ BlazorValidationsDemo/Helpers/BsFieldClassProvider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace BlazorValidationsDemo.Helpers
+{
+    public class BsFieldClassProvider : FieldCssClassProvider
+    {
+        public override string GetFieldCssClass(EditContext editContext, in FieldIdentifier _fieldIdentifier)
+        {
+            var isValid = !editContext.GetValidationMessages(_fieldIdentifier).Any();
+
+            //var _field = fieldIdentifier.
+
+            var isModified = editContext.IsModified(_fieldIdentifier);
+
+            if (editContext.IsModified(_fieldIdentifier))
+            {
+                return isValid ? "modified is-valid" : "modified is-invalid";
+            }
+            else
+            {
+                return isValid ? "is-valid" : "is-invalid";
+            }
+        }
+
+        
+
+    }
+    public class Student
+    {
+        public string Name { get; set; }
+        public bool Enrolled { get; set; }
+    }
+
+}
